838294f5beae74820f45ed96d938adf2740526b2
YaweiYe29 <yye@student.ethz.ch>
2017-07-06 22:08:13 +0200

    Modified RobotCar database class

--- datasets/dbOxfordRobotCar.m
+++ datasets/dbOxfordRobotCar.m
@@ -1,5 +1,9 @@
 classdef dbOxfordRobotCar < dbBase
     
+    properties
+        dbTimeStamp, qTimeStamp
+    end
+    
     methods
         function db= dbOxfordRobotCar(whichSet)
             % fullSize is: true or false
@@ -7,15 +11,37 @@ classdef dbOxfordRobotCar < dbBase
             
             assert( ismember(whichSet, {'train', 'val', 'test'}) );
             
-            db.name= sprintf('robotcar_%s', whichSet);
+            db.name= sprintf('robotCar_%s', whichSet);
             
             paths= localPaths();
-            dbRoot= paths.dsetRootPitts;
-            db.dbPath= [dbRoot, 'images/'];
-            db.qPath= [dbRoot, 'queries/'];
+            dbRoot= paths.dsetRootRobotCar;
+            db.dbPath= dbRoot;
+            db.qPath= dbRoot;
             
             db.dbLoad();
         end
+        
+        function posIDs= nontrivialPosQ(db, iQuery)
+            [posIDs, dSq]= db.cp.getPosIDs(db.utmQ(:,iQuery));
+            posIDs= posIDs(dSq>1 & dSq<=db.nonTrivPosDistSqThr & ...
+                db.qTimeStamp(iQuery)~=db.dbTimeStamp(posIDs) );
+        end
+        
+        function posIDs= nontrivialPosDb(db, iDb)
+            [posIDs, dSq]= db.cp.getPosDbIDs(iDb);
+            posIDs= posIDs(dSq>1 & dSq<=db.nonTrivPosDistSqThr & ...
+                db.dbTimeStamp(iDb)~=db.dbTimeStamp(posIDs) );
+        end
+        
+        function [ids, ds]= nnSearchPostprocess(db, searcher, iQuery, nTop)
+            % there's roughly up to 5 panoramas at different times per
+            % location, so a very conservative estimate..
+            [ids, ds]= searcher(iQuery, nTop*10);
+            keep= db.qTimeStamp(iQuery)~=db.dbTimeStamp(ids);
+            ids= ids(keep);
+            ds= ds(keep);
+            ids= ids(1:min(end,nTop));
+            ds= ds(1:min(end,nTop));
+        end
     end
 end
-
