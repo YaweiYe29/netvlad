e25ad3ff347f80b40183a4c9f10b30403801f0d6
YaweiYe29 <yye@student.ethz.ch>
2017-07-06 22:10:20 +0200

    Minor

--- plot_function/plotPositions2D.m
+++ plot_function/plotPositions2D.m
@@ -2,5 +2,5 @@ function plotPositions2D(positions)
 % co = get(gca,'ColorOrder'); % Initial
 % % Change to new colors.
 % set(gca, 'ColorOrder', co(2:end, :), 'NextPlot', 'replacechildren');
-plot(positions(:, 2), positions(:, 1), '.', 'MarkerSize', 8);
+plot(positions(:, 2), positions(:, 1), '.', 'MarkerSize', 4);
 end
